5cf43a1db6ae710f284a365368c55b5f686eb9e3
WajithAAA <azeeswajid@gmail.com>
2023-09-06 21:14:05 +0530

    ml project dir

---
+++ the-green/static/js/script.js
@@ -0,0 +1,40 @@
+// --- js for toggle menu ---
+
+var MenuItems = document.getElementById('MenuItems');
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(MenuItems.style.maxHeight == "0px")
+        {
+        MenuItems.style.maxHeight = "200px";
+        }
+    else
+        {
+            MenuItems.style.maxHeight = "0px";  
+        }
+}
+
+
+// ----- product details page image change ---- 
+
+var ProductImg = document.getElementById('ProductImg');
+var SmallImg = document.getElementsByClassName('SmallImg');
+
+SmallImg[0].onclick = function(){
+    ProductImg.src =SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function(){
+    ProductImg.src =SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function(){
+    ProductImg.src =SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function(){
+    ProductImg.src =SmallImg[3].src;
+}
+
+
